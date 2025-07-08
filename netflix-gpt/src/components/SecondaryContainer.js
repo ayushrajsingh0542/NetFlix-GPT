7c14ff83450d22aab95719b773e1eaf74417cb3a
ayushrajsingh0542 <aayushrajsingh11@gmail.com>
2025-07-08 20:57:00 +0530

    removed play and info buttons

--- netflix-gpt/src/components/SecondaryContainer.js
+++ netflix-gpt/src/components/SecondaryContainer.js
@@ -5,8 +5,8 @@ function SecondaryContainer() {
     const movies=useSelector((store)=>store.movies)
     return ( 
         <div className="bg-black">
-            <div className="mt-0 md:-mt-52 relative z-20 pl-4 md:pl-12">
-            <MovieList title={"Now Playing"} movies_={movies?.nowPlayingMovies}/>
+            <div className="mt-0 md:-mt-80 relative z-20 pl-4 md:pl-12">
+            <MovieList title={""} movies_={movies?.nowPlayingMovies}/>
          
             <MovieList title={"Top Rated Movies"} movies_={movies?.topRatedMovies}/>
             <MovieList title={"Popular"} movies_={movies?.popularMovies}/>
--- netflix-gpt/src/components/VideoTitle.js
+++ netflix-gpt/src/components/VideoTitle.js
@@ -1,16 +1,16 @@
 function VideoTitle({ title, overview }) {
   return (
-    <div className="w-screen aspect-video pt-[22%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
+    <div className="w-screen aspect-video pt-[22%] px-6 md:px-18 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-2xl md:text-6xl ml-4 font-bold">{title}</h1>
       <p className="hidden md:inline-block p-6 text-lg w-1/4">{overview}</p>
-      <div className="my-4 md:m-0">
+      {/* <div className="my-4 md:m-0">
         <button className="bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl rounded-lg hover:bg-opacity-80">
           <i class="fa-solid fa-play"></i>&nbsp;&nbsp;Play
         </button>
         <button className="hidden md:inline-block mx-2 bg-gray-500 text-white p-4 px-12 text-xl bg-opacity-80 rounded-lg mx-2 my-2 hover:bg-opacity-100"><i class="fa-solid fa-circle-info"></i>
           &nbsp;&nbsp;More Info
         </button>
-      </div>
+      </div> */}
     </div>
   );
 }
