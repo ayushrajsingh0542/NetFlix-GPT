10655fd11556b61a67edd9e22bc55905b39a9e52
ayushrajsingh0542 <aayushrajsingh11@gmail.com>
2025-07-11 21:54:43 +0530

    bug fixed

--- netflix-gpt/src/components/VideoTitle.js
+++ netflix-gpt/src/components/VideoTitle.js
@@ -2,7 +2,7 @@ function VideoTitle({ title, overview }) {
   return (
     <div className="w-screen aspect-video pt-[22%] px-6 md:px-18 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-2xl md:text-6xl ml-4 font-bold">{title}</h1>
-      <p className="hidden md:inline-block p-6 text-lg w-1/4">{overview}</p>
+      <p className="hidden md:inline-block p-6 text-lg w-1/4">{overview.length>250?overview.slice(0,150)+"...":overview}</p>
       {/* <div className="my-4 md:m-0">
         <button className="bg-white text-black py-1 md:py-4 px-3 md:px-12 text-xl rounded-lg hover:bg-opacity-80">
           <i class="fa-solid fa-play"></i>&nbsp;&nbsp;Play
